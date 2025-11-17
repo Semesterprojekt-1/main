22ea0d5cbb3b53b899c869918beb8090f2bc1151
apram2575-oss <apram2575@gmail.com>
2025-11-17 14:21:32 +0100

    Integrate OLED display for robot speed monitoring
    Added I2C and OLED display functionality to show robot speed.

--- portfolio 4/access_pointVictorsNetwork.py
+++ portfolio 4/access_pointVictorsNetwork.py
@@ -1,14 +1,19 @@
 import network
 import socket
 import time
-from machine import ADC,Pin
+from machine import ADC,Pin, I2C
 import math
+import ssd1306
 
 
 
 SSID = "Victor's Galaxy A52"
 PASSWORD = "ebpm3056"
 PORT = 1234
+
+#SSID = "AndroidAPa138"
+#PASSWORD = "shle9732"
+#PORT = 1234
 
 # Connect server Pico to Wi-Fi
 wlan = network.WLAN(network.STA_IF)
@@ -38,6 +43,8 @@ print(f"Server listening on port {PORT}...")
 # Defining the adc's for x and y
 adcX = ADC(26)
 adcY = ADC(27)
+i2c = I2C(0, scl=Pin(1), sda=Pin(0))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 while True:
     try:
@@ -53,12 +60,30 @@ while True:
                 message =  f"{valueX},{valueY}\n"  #We are trying to send the x- and y values here
                 conn.send((message + "\n").encode())
                 print(f"Sent: {message}")
-                time.sleep(0.1)
+                time.sleep(0.15)
+                
+                if (valueX - 50500)<0:
+                    speedX = (valueX - 50500) / 24000*100
+                else:
+                    speedX = (valueX - 50500) / 14000*100
+                    
+                if (valueY - 50300)<0:
+                    speedY = (valueY - 50300) / 24000*100
+                else:
+                    speedY = (valueY - 50300) / 14000*100
+                    
+                speed = math.sqrt(speedX**2 + speedY**2)
+                
+                oled.fill(0)
+                oled.text("Robot Speed:", 0, 0)
+                oled.text("{:.1f}%".format(speed), 0, 20)
+                oled.show()
+
             except OSError:
                 print(f"Client {client_addr} disconnected")
                 conn.close()
                 break
 
-    except Exception as e:
-        print("Server error:", e)
-        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Server closed.")
+        break
