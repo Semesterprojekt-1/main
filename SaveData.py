8a50ba4c88333776f76585c259f6c7a4e5451023
Mr. O'Kane <104971960+Mr-OKane@users.noreply.github.com>
2025-12-28 18:37:40 +0100

    Delete SaveData.py

--- SaveData.py
+++
@@ -1,8 +0,0 @@
-
-
-def SaveData(Data):
-    with open('data_log.csv' , 'w') as f:     
-        for item in Data:
-            # Append the data to the file
-            f.write(str(item) + "\n") #Writes item into csv file
-            f.flush() # Ensure data is written to the file
